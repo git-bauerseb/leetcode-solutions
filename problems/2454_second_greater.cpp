a767ab4caeae77d59cab064136e3fb67abaf4b67
git-bauerseb <48291831+git-bauerseb@users.noreply.github.com>
2023-01-15 10:31:34 +0100

    Better solution for 2454

--- problems/2454_second_greater.cpp
+++ problems/2454_second_greater.cpp
@@ -2,6 +2,12 @@
 
 using namespace std;
 
+struct Comparator {
+    bool operator()(pair<int,int>& a, pair<int,int>& b) {
+        return a.first > b.first;
+    }
+};
+
 /*
     2,4,0,9,6
 
@@ -18,30 +24,26 @@ class Solution {
             const int n = nums.size();
 
             vector<int> result(n, -1);
-            set<int> indc;
+            priority_queue<pair<int,int>, vector<pair<int,int>>, Comparator> pq;
+
 
             // Save indices of elements less than the current element
             stack<int> mon_stack;
 
+            // Error! Dont compare indices for greater
+
             // O(n)
             for (int i = 0; i < n; i++) {
-
-                if (indc.size() > 0) {
-                    for (auto it = indc.begin(); it != indc.end();) {
-                        if (nums[*it] < nums[i]) {
-                            result[*it] = nums[i];
-                            it = indc.erase(it);
-                        } else {
-                            it++;
-                        }
-                    }
+                while (!pq.empty()) {
+                    auto top = pq.top();
+                    if (top.first >= nums[i]) {break;}
+                    else {pq.pop(); result[top.second] = nums[i];}
                 }
 
-                while (mon_stack.size() > 0 
-                    && nums[mon_stack.top()] < nums[i]) {
+                while (mon_stack.size() > 0 && nums[mon_stack.top()] < nums[i]) {
                     int top_idx = mon_stack.top();
                     mon_stack.pop();
-                    indc.insert(top_idx);
+                    pq.push({nums[top_idx], top_idx});
                 }
                 mon_stack.push(i);
             }
@@ -51,13 +53,16 @@ class Solution {
 };
 
 int main() {
+    
     // -1 -1 3 3 -1 -1
-    vector<int> nums{2,4,0,9,6};
+    vector<int> nums{11,13,15,12,0,15,12,11,9};
 
     Solution solution{};
     auto result = solution.secondGreaterElement(nums);
 
     for (auto& e : result) {cout << e << " ";}
     cout << "\n";
+    
+
     return 0;
 }
